2eb80ad6b727f3a1d2d09a7c9c2ba1df32037ece
piatek29535 <adamex12343@gmail.com>
2022-09-02 20:41:07 +0200

    template layouts added

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+posts = [
+    {
+        "title":"Title",
+        "desc":"Post 1 Post 1",
+        "tag":"Post 1"
+    },
+    {
+        "title":"Title",
+        "desc":"Post 2 Post 2",
+        "tag":"Post 2"
+    },{
+        "title":"Title",
+        "desc":"Post 3 Post 3",
+        "tag":"Post 3"
+    },
+]
+
+@app.route('/')
 @app.route("/home")
 def home():
     #home pagge title 
     title = "Home"
 
-    return render_template('home.html', title=title)
-
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
