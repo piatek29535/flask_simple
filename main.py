feda43fc0e041fa3ae53bd61f162719ed2b2310f
piatek29535 <adamex12343@gmail.com>
2022-09-02 20:04:48 +0200

    Back to the roots

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "H"
+@app.route("/home")
+def home():
+    #home pagge title 
+    title = "Home"
+
+    return render_template('home.html', title=title)
+
+
+@app.route("/about")
+def about():
+    #about page title
+    title = "About"
+
+    return render_template("about.html", title=title)
+
+if __name__ == "__main__":
+    app.run(debug=True)
